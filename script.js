b618c16b9d52d88bc7178b0e021c4784ede46abf
PERO <h.natsuki621.design@gmail.com>
2026-06-04 14:49:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (!header) return;
+
+  if (window.scrollY > 50) {
+    header.classList.add("shrink");
+  } else {
+    header.classList.remove("shrink");
+  }
+});
+
 
 /* =========================
    ② IntersectionObserver
