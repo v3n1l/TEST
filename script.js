ce2942f5d54ee9ed855221b40083a5e4c55bf9dd
PERO <h.natsuki621.design@gmail.com>
2026-06-04 10:46:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document.querySelectorAll(".fade").forEach((el)=>{
+
+observer.observe(el);
+
+});
