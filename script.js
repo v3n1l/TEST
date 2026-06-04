808ee6555fb4c9641f0b4c03ffc957ab78ecfae3
PERO <h.natsuki621.design@gmail.com>
2026-06-04 14:28:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ document.querySelectorAll(".fade").forEach((el)=>{
 observer.observe(el);
 
 });
+
+window.addEventListener("load",()=>{
+  const load = document.getElementById("loading");
+
+  setTimeout(()=>{
+    load.style.opacity = "0";
+    load.style.transition = "0.6s";
+
+    setTimeout(()=>{
+      load.style.display = "none";
+    },600);
+
+  },800);
+});
